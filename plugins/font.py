74e290740ac3062e0827fbd7cf1ab7a9aadb5ecc
Amit <heroku467@gmail.com>
2025-02-23 00:23:08 +0530

    Update font.py

--- plugins/font.py
+++ plugins/font.py
@@ -4,7 +4,7 @@
 
 
 import os
-from plugins.Extra.fotnt_string import Fonts
+from plugins.fotnt_string import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
