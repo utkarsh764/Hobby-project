82b6a453632f69681efff1936e2534ab7e318bba
Amit <heroku467@gmail.com>
2025-02-23 00:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,12 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$') 
 
 API_ID = os.environ.get("API_ID", "")
-
 API_HASH = os.environ.get("API_HASH", "")
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
 DB_NAME = os.environ.get("DB_NAME","")     
-
 DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
