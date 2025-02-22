5244d838d64d4a8c9b1404cb55c6652b2359dac1
Amit <heroku467@gmail.com>
2025-02-23 00:46:16 +0530

    Update sticker.py

--- plugins/sticker.py
+++ plugins/sticker.py
@@ -1,13 +1,14 @@
+
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("stickerid") & filters.reply)
+@Client.on_message(filters.command("stickerid"))
 async def stickerid(bot, message):
     replied_msg = message.reply_to_message
-    
+
     if replied_msg and replied_msg.sticker:
         await message.reply_text(
             f"🆔 **Sticker ID:**  \n\n`{replied_msg.sticker.file_id}`\n\n"
             f"🔑 **Unique ID:** `{replied_msg.sticker.file_unique_id}`"
         )
     else:
-        await message.reply_text("ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ ᴡɪᴛʜ /stickerid 🖼️")
+        await message.reply_text("**⚠️ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ ᴡɪᴛʜ /stickerid **")
