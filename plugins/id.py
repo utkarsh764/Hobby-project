b072d4bc2907852057215f9b43fbbc8ea8a7e45c
Amit <heroku467@gmail.com>
2025-02-24 14:34:05 +0530

    Create id.py

---
+++ plugins/id.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from bot import Bot  # Import the Bot class from the main bot file
+
+@Bot.on_message(filters.command("id"))
+async def id_command(client: Bot, message: Message):
+    """
+    Handler for the /id command.
+    Sends the chat ID of the current chat or user.
+    """
+    # Determine the chat title or user's full name
+    if message.chat.title:
+        chat_title = message.chat.title
+    else:
+        chat_title = message.from_user.full_name
+
+    # Prepare the response text
+    id_text = f"**Chat ID of** {chat_title} **is** `{message.chat.id}`"
+
+    # Send the response
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=id_text,
+        reply_to_message_id=message.id,
+    )
