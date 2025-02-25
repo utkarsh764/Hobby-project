da1bc29400302f38a6bd7a65884fa297a4e39acf
Amit <heroku467@gmail.com>
2025-02-25 14:42:18 +0530

    Update pdf (1).py

--- plugins/pdf (1).py
+++ plugins/pdf (1).py
@@ -237,7 +237,7 @@ async def handle_image_metadata(client: Client, message: Message):
 async def merge_files(client: Client, message: Message):
     await merge_plugin.merge_files(client, message)
 
-@Client.on_message(filters.text & filters.private & ~filters.command(["start", "id", "set", "telegraph", "stickerid", "accept", "users", "broadcast"]))
+@Client.on_message(filters.text & filters.private & ~filters.command(["start", "id", "set", "telegraph", "stickerid", "accept", "users", "broadcast", "rename"]))
 async def handle_filename(client: Client, message: Message):
     user_id = message.from_user.id
     if user_id in merge_plugin.user_states and merge_plugin.user_states[user_id] == "waiting_for_filename":
