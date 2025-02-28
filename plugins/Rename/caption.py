24012d5fdc63121862dcb3ce291e285ada75463d
Amit <heroku467@gmail.com>
2025-02-28 11:12:43 +0530

    Update caption.py

--- plugins/Rename/caption.py
+++ plugins/Rename/caption.py
@@ -1,10 +1,7 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client, filters 
 from database.users_chats_db import db
-from info import RENAME_MODE
+from config import RENAME_MODE
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
