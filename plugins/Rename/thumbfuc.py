2c58ca423f1a9fb0c5e44ca1e5765a49839d00df
Amit <heroku467@gmail.com>
2025-02-28 11:19:34 +0530

    Update thumbfuc.py

--- plugins/Rename/thumbfuc.py
+++ plugins/Rename/thumbfuc.py
@@ -1,6 +1,6 @@
 
 from pyrogram import Client, filters, enums
-from database.users_chats_db import db
+from helper.database import db
 from config import RENAME_MODE
 
 @Client.on_message(filters.private & filters.command(['view_thumb']))
