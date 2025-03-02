dee7591130d39bf126fb645810e1edad167037e3
Amit <heroku467@gmail.com>
2025-03-03 02:21:13 +0530

    Update utils.py

--- plugins/Rename/utils.py
+++ plugins/Rename/utils.py
@@ -26,8 +26,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]))
+            ''.join(["●" for i in range(math.floor(percentage / 5))]),
+            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]))
             
         tmp = progress + script.PROGRESS_BAR.format( 
             round(percentage, 2),
