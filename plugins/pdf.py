f26e13cf49584e8b41991394f8e8a153c25567c1
Amit <heroku467@gmail.com>
2025-03-09 17:00:56 +0530

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -13,7 +13,7 @@ from filters import user_filter
 
 logger = logging.getLogger(__name__)
 
-MAX_FILE_SIZE = 20 * 1024 * 1024  # 20MB
+MAX_FILE_SIZE = 500 * 1024 * 1024  # 20MB
 
 class MergePlugin:
     def __init__(self):
