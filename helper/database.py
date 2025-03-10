22ce6a28a018f1a6fbcfa5ad9bde909110c95a92
Amit <heroku467@gmail.com>
2025-03-10 16:05:31 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -6,46 +6,47 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.user
+        self.users = self.db.user  # Collection for users
+        self.channels = self.db.channels  # Collection for channels
 
     def new_user(self, id):
         return dict(
-            _id=int(id),                                   
+            _id=int(id),
             file_id=None,
             caption=None
         )
 
     async def add_user(self, id):
         user = self.new_user(id)
-        await self.col.insert_one(user)
+        await self.users.insert_one(user)
 
     async def is_user_exist(self, id):
-         user = await self.col.find_one({'_id': int(id)})
-         return bool(user)
+        user = await self.users.find_one({'_id': int(id)})
+        return bool(user)
 
     async def total_users_count(self):
-        count = await self.col.count_documents({})
+        count = await self.users.count_documents({})
         return count
 
     async def get_all_users(self):
-        all_users = self.col.find({})
+        all_users = self.users.find({})
         return all_users
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'_id': int(user_id)})
-    
+        await self.users.delete_many({'_id': int(user_id)})
+
     async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+        await self.users.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
     async def get_thumbnail(self, id):
-        user = await self.col.find_one({'_id': int(id)})
+        user = await self.users.find_one({'_id': int(id)})
         return user.get('file_id', None)
 
     async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+        await self.users.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
 
     async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
+        user = await self.users.find_one({'_id': int(id)})
         return user.get('caption', None)
 
     # Store the broadcast message ID
@@ -53,7 +54,7 @@ class Database:
         await self.users.update_one(
             {"_id": user_id}, {"$set": {"last_broadcast_msg": message_id}}, upsert=True
         )
-        
+
     # Retrieve the last broadcast message ID
     async def get_broadcast_message(self, user_id):
         user = await self.users.find_one({"_id": user_id})
@@ -63,4 +64,29 @@ class Database:
     async def delete_broadcast_message(self, user_id):
         await self.users.update_one({"_id": user_id}, {"$unset": {"last_broadcast_msg": ""}})
 
+    # =================== CHANNEL SYSTEM ===================
+
+    # Add a channel to the database
+    async def add_channel(self, channel_id):
+        if not await self.channels.find_one({"_id": channel_id}):
+            await self.channels.insert_one({"_id": channel_id})
+            return True  # New channel added
+        return False  # Channel already exists
+
+    # Remove a channel from the database
+    async def remove_channel(self, channel_id):
+        await self.channels.delete_one({"_id": channel_id})
+
+    # Check if a channel exists
+    async def is_channel_exist(self, channel_id):
+        channel = await self.channels.find_one({"_id": channel_id})
+        return bool(channel)
+
+    # Get all stored channels
+    async def get_all_channels(self):
+        channels_cursor = self.channels.find({})
+        return [channel["_id"] async for channel in channels_cursor]
+
+
+# Initialize Database
 db = Database(DB_URL, DB_NAME)
