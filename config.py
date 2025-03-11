f42e11d90e2c23d28e56c5fb1750eed96443cf5b
Amit <heroku467@gmail.com>
2025-03-11 19:54:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ API_ID = os.environ.get("API_ID", "22012880")
 API_HASH = os.environ.get("API_HASH", "5b0e07f5a96d48b704eb9850d274fe1d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
-
 DB_NAME = os.environ.get("DB_NAME","Z900")     
 DB_URL = os.environ.get("DB_URL","")
 
@@ -21,11 +19,15 @@ RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or False
 
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', False))
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002027394591"))
+
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
 REACTIONS = ["🤝", "😇", "🤗", "😍", "🎅", "🥰", "🤩", "😘", "😛", "😈", "🎉", "🫡", "😎", "🔥", "🤭", "🌚", "🆒", "👻", "😁"] #don't add any emoji because tg not support all emoji reactions
 
-
+FORCE_SUB_CHANNEL1 = os.environ.get("F_SUB1", "-1002385466192")
+FORCE_SUB_CHANNEL2 = os.environ.get("F_SUB2", "")
+FORCE_SUB_CHANNEL3 = os.environ.get("F_SUB3", "")
+FORCE_SUB_CHANNEL4 = os.environ.get("F_SUB4", "")
 
 
 #new
@@ -35,5 +37,3 @@ F_SUB_1 = int(os.environ.get("F_SUB_1", "-1002481520987"))#put 0 to disable
 F_SUB_2 = int(os.environ.get("F_SUB_2", "0"))#put 0 to disable
 F_SUB_3 = int(os.environ.get("F_SUB_3", "0"))#put 0 to disable
 F_SUB_4 = int(os.environ.get("F_SUB_4", "0"))#put 0 to disable
-
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
