36d7479c054a079c5f4beb635a3ac954c1a81e97
Amit <heroku467@gmail.com>
2025-03-12 11:55:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 import logging
 import logging.config
-import asyncio
-from pyrogram import Client
-from pyrogram.enums import ParseMode
-from aiohttp import web
-from datetime import datetime
-import sys
+from pyrogram import Client 
 from config import *
+from aiohttp import web
 from plugins.web_support import web_server
 
 logging.config.fileConfig('logging.conf')
@@ -15,6 +11,7 @@ logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
             name="renamer",
@@ -31,39 +28,17 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username
-        self.uptime = datetime.now()
-
-        # Force Subscription Logic for 4 Channels
-        for index, channel in enumerate([FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4], start=1):
-            if channel:
-                try:
-                    link = (await self.get_chat(channel)).invite_link
-                    if not link:
-                        await self.export_chat_invite_link(channel)
-                        link = (await self.get_chat(channel)).invite_link
-                    setattr(self, f"invitelink{index}", link)
-                except Exception as e:
-                    logging.warning(e)
-                    logging.warning(f"Bot can't export invite link for FORCE_SUB_CHANNEL{index}!")
-                    logging.warning(f"Make sure bot is admin in the channel with 'Invite Users via Link' permission.")
-                    sys.exit()
-
-        # Start Web Server
+        
         app = web.AppRunner(await web_server())
         await app.setup()
-        await web.TCPSite(app, "0.0.0.0", PORT).start()
-
-        # Notify Owner
-        try:
-            await self.send_message(ADMIN, text=f"<b><blockquote>🤖 Bot Restarted!</blockquote></b>")
-        except Exception as e:
-            logging.warning(f"Failed to notify owner: {e}")
-
-        logging.info(f"{me.first_name} ✅✅ Bot started successfully ✅✅")
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+        
+        logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
 
     async def stop(self, *args):
-        await super().stop()
+        await super().stop()      
         logging.info("Bot Stopped 🙄")
-
+        
 bot = Bot()
 bot.run()
