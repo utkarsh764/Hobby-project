1244139f9219b377b48be9926aadc995e3e37cdf
utkarsh764 <utkarshsrivastavalala123@gmail.com>
2025-03-13 20:27:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = os.environ.get("API_HASH", "9b2fac908fb7f9a3dabac3b0a57211b1")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7786105206:AAHm0iceXktLQugF_5xs-lRj8xjbtMR-xE0") 
 
 DB_NAME = os.environ.get("DB_NAME","Utkarsh9")     
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Utkarsh123:9335924360@utkarsh9.af91n.mongodb.net/?retryWrites=true&w=majority&appName=Utkarsh9")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Utkarsh123:<db_password>@utkarsh9.af91n.mongodb.net/?retryWrites=true&w=majority&appName=Utkarsh9")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1214167849').split()]
 PORT = os.environ.get("PORT", "8080")
