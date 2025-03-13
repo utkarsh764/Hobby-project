6b47ab1045386336e299d1d2bdeaffe2b503139e
utkarsh764 <utkarshsrivastavalala123@gmail.com>
2025-03-13 19:47:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,6 +21,6 @@ SESSION_STRING = os.environ.get("SESSION_STRING", "")
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002348383946').split()] #Ex : ('-10073828 -102782829 -1007282828')
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "--1002354465117"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002354465117"))
 
 REACTIONS = ["🤝", "😇", "🤗", "😍", "🎅", "🥰", "🤩", "😘", "😛", "😈", "🎉", "🫡", "😎", "🔥", "🤭", "🌚", "🆒", "👻", "😁"] #don't add any emoji because tg not support all emoji reactions
