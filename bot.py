791da39ed5b278970098bd148daa89a40856e5a1
Amit <heroku467@gmail.com>
2025-03-13 12:01:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 from config import *
 from aiohttp import web
 from plugins.web_support import web_server
+from plugins.Fsub import check_subscription
 
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
