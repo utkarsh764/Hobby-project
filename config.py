66da2ee5e41c1564cfc65a9b2eccbe2a8ab76cab
Amit <heroku467@gmail.com>
2025-03-13 13:01:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "22012880")
-API_HASH = os.environ.get("API_HASH", "5b0e07f5a96d48b704eb9850d274fe1d")
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 DB_NAME = os.environ.get("DB_NAME","Z900")     
-DB_URL = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URL = os.environ.get("DB_URL","")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6803505727').split()]
 PORT = os.environ.get("PORT", "6969")
